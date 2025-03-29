3d42747faf656dd86d8eb262b7163d88a2c4561e
Dhruv Kapoor <kapoor_1992@hotmail.com>
2025-03-28 21:10:02 -0400

    added script

---
+++ papa.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+import time
+
+_TEMP_DIR = '/home/dhruv/Documents/papa/temp'
+_FINAL_DIR = '/home/dhruv/Documents/papa/final'
+_YT_URL = 'https://www.youtube.com/watch?v=dH3uanfPFTs'
+
+def _create_instrumental_video() -> None:
+    os.system(f'yt-dlp -f bestaudio --extract-audio --audio-format wav --audio-quality 0 -o "{_TEMP_DIR}/yt_audio.wav" {_YT_URL}')
+    os.system(f'yt-dlp -f bestvideo -o "{_TEMP_DIR}/yt_video.webm" {_YT_URL}')
+
+    os.system(f'demucs "{_TEMP_DIR}/yt_audio.wav" -o "{_TEMP_DIR}/"')
+
+    os.system(f'ffmpeg -i "{_TEMP_DIR}/htdemucs/yt_audio/bass.wav" -i "{_TEMP_DIR}/htdemucs/yt_audio/drums.wav" -i "{_TEMP_DIR}/htdemucs/yt_audio/other.wav" -filter_complex [0:a:0][1:a:0]amix=inputs=3:duration=longest[aout] -map [aout] "{_TEMP_DIR}/instrumental.wav"')
+
+    os.system(f'ffmpeg -i "{_TEMP_DIR}/yt_video.webm" -i "{_TEMP_DIR}/instrumental.wav" -c:v copy -c:a aac "{_FINAL_DIR}/{str(int(time.time()))}.mp4"')
+
+def _main() -> None:
+    os.makedirs(_TEMP_DIR, exist_ok=True)
+    os.makedirs(_FINAL_DIR, exist_ok=True)
+
+    _create_instrumental_video()
+    
+    shutil.rmtree(_TEMP_DIR)
+
+if __name__ == '__main__':
+    _main()
