e3114f62e99c01633a941fe02d3a21cadb8d4ec8
Dhruv Kapoor <kapoor_1992@hotmail.com>
2025-03-30 14:33:11 -0700

    os-independent pathing

--- papa.py
+++ papa.py
@@ -1,20 +1,21 @@
 import argparse
 import os
+os.path import sep
 import shutil
 import time
 
-_TEMP_DIR = f'{os.getcwd()}/temp'
-_FINAL_DIR = f'{os.getcwd()}/final'
+_TEMP_DIR = f'{os.getcwd()}{sep}temp'
+_FINAL_DIR = f'{os.getcwd()}{sep}final'
 
 def _create_instrumental_video(yt_url: str) -> None:
-    os.system(f'yt-dlp -f bestaudio --extract-audio --audio-format wav --audio-quality 0 -o "{_TEMP_DIR}/yt_audio.wav" {yt_url}')
-    os.system(f'yt-dlp -f bestvideo -o "{_TEMP_DIR}/yt_video.webm" {yt_url}')
+    os.system(f'yt-dlp -f bestaudio --extract-audio --audio-format wav --audio-quality 0 -o "{_TEMP_DIR}{sep}yt_audio.wav" {yt_url}')
+    os.system(f'yt-dlp -f bestvideo -o "{_TEMP_DIR}{sep}yt_video.webm" {yt_url}')
 
-    os.system(f'demucs "{_TEMP_DIR}/yt_audio.wav" -o "{_TEMP_DIR}/"')
+    os.system(f'demucs "{_TEMP_DIR}{sep}yt_audio.wav" -o "{_TEMP_DIR}{sep}"')
 
-    os.system(f'ffmpeg -i "{_TEMP_DIR}/htdemucs/yt_audio/bass.wav" -i "{_TEMP_DIR}/htdemucs/yt_audio/drums.wav" -i "{_TEMP_DIR}/htdemucs/yt_audio/other.wav" -filter_complex [0:a:0][1:a:0]amix=inputs=3:duration=longest[aout] -map [aout] "{_TEMP_DIR}/instrumental.wav"')
+    os.system(f'ffmpeg -i "{_TEMP_DIR}{sep}htdemucs{sep}yt_audio{sep}bass.wav" -i "{_TEMP_DIR}{sep}htdemucs{sep}yt_audio{sep}drums.wav" -i "{_TEMP_DIR}{sep}htdemucs{sep}yt_audio{sep}other.wav" -filter_complex [0:a:0][1:a:0]amix=inputs=3:duration=longest[aout] -map [aout] "{_TEMP_DIR}{sep}instrumental.wav"')
 
-    os.system(f'ffmpeg -i "{_TEMP_DIR}/yt_video.webm" -i "{_TEMP_DIR}/instrumental.wav" -c:v copy -c:a aac "{_FINAL_DIR}/{str(int(time.time()))}.mp4"')
+    os.system(f'ffmpeg -i "{_TEMP_DIR}{sep}yt_video.webm" -i "{_TEMP_DIR}{sep}instrumental.wav" -c:v copy -c:a aac "{_FINAL_DIR}{sep}{str(int(time.time()))}.mp4"')
 
 def _main(yt_url: str) -> None:
     os.makedirs(_TEMP_DIR, exist_ok=True)
