b59a5b1525c62e3bdb8b512fb93780dc1c703784
Dhruv Kapoor <kapoor_1992@hotmail.com>
2025-03-30 18:20:48 -0400

    error catching

--- papa.py
+++ papa.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-os.path import sep
+from os.path import sep
 import shutil
 import time
 
@@ -10,7 +10,7 @@ _FINAL_DIR = f'{os.getcwd()}{sep}final'
 def _create_instrumental_video(yt_url: str) -> None:
     os.system(f'yt-dlp -f bestaudio --extract-audio --audio-format wav --audio-quality 0 -o "{_TEMP_DIR}{sep}yt_audio.wav" {yt_url}')
     os.system(f'yt-dlp -f bestvideo -o "{_TEMP_DIR}{sep}yt_video.webm" {yt_url}')
-
+    
     os.system(f'demucs "{_TEMP_DIR}{sep}yt_audio.wav" -o "{_TEMP_DIR}{sep}"')
 
     os.system(f'ffmpeg -i "{_TEMP_DIR}{sep}htdemucs{sep}yt_audio{sep}bass.wav" -i "{_TEMP_DIR}{sep}htdemucs{sep}yt_audio{sep}drums.wav" -i "{_TEMP_DIR}{sep}htdemucs{sep}yt_audio{sep}other.wav" -filter_complex [0:a:0][1:a:0]amix=inputs=3:duration=longest[aout] -map [aout] "{_TEMP_DIR}{sep}instrumental.wav"')
@@ -21,9 +21,12 @@ def _main(yt_url: str) -> None:
     os.makedirs(_TEMP_DIR, exist_ok=True)
     os.makedirs(_FINAL_DIR, exist_ok=True)
 
-    _create_instrumental_video(yt_url)
-    
-    shutil.rmtree(_TEMP_DIR)
+    try:
+        _create_instrumental_video(yt_url)
+    except Exception as _:
+        pass
+    finally:
+        shutil.rmtree(_TEMP_DIR)
 
 if __name__ == '__main__':
     _parser = argparse.ArgumentParser()
